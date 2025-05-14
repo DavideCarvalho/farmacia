9b5795a839d5cd58ff3f0648ddfa0b7d6df1e89e
Davi de Carvalho <davi_carvalho96@hotmail.com>
2025-05-14 11:32:06 -0300

    tentando arrumar permissão

--- docker/easypanel/entrypoint.sh
+++ docker/easypanel/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+chmod -R gu+w /app/storage
+chmod -R guo+w /app/storage
+chmod -R gu+w /app/bootstrap/cache
+chmod -R guo+w /app/bootstrap/cache
+php artisan cache:clear
+
 # Run migrations
 php artisan migrate --force
 
-# echo "Running in production mode"
+exec "$@"
