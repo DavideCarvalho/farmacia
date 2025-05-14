b4d95fe0aca2ca030220297a68e14d3aef942153
Davi de Carvalho <davi_carvalho96@hotmail.com>
2025-05-14 11:40:10 -0300

    arrumando comandos de cache

--- docker/easypanel/entrypoint.sh
+++ docker/easypanel/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-chmod -R gu+w /app/storage
-chmod -R guo+w /app/storage
-chmod -R gu+w /app/bootstrap/cache
-chmod -R guo+w /app/bootstrap/cache
-php artisan cache:clear
-
 # Run migrations
 php artisan migrate --force
 
