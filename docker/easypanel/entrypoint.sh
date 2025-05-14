4c16e534fef1f4996411313a984a68ddcc3e52f9
Davi de Carvalho <davi_carvalho96@hotmail.com>
2025-05-14 09:17:53 -0300

    tentando arrumar

--- docker/easypanel/entrypoint.sh
+++ docker/easypanel/entrypoint.sh
@@ -3,44 +3,15 @@
 # Set the base directory for the app
 BASEDIR=/opt/laravel
 
-# Flag to check if it's a fresh install
-FIRST_INSTALL=false
-
-# Check if the .env file exists
-if [ ! -f "$BASEDIR/.env" ]; then
-   FIRST_INSTALL=true
-fi
-
-# Use FIRST_INSTALL variable as needed in the rest of your script
-if [ "$FIRST_INSTALL" = true ]; then
-    echo "This is a FRESH INSTALL."
-
-    # Determine the appropriate environment file based on the PRODUCTION variable
-    if [ "$PRODUCTION" = "1" ]; then
-        ENV_FILE=".env.production"
-    else
-        ENV_FILE=".env.example"
-    fi
-
-    # Copy the chosen environment file to create the .env file
-    echo "Generating .env from a copy $ENV_FILE ..."
-    cp $ENV_FILE .env
-    echo "File .env generated."
-fi
-
 # Clear the old boostrap/cache
 php artisan clear-compiled
 
 # Install composer dependencies
-if [ "$PRODUCTION" = "1" ]; then
-    composer install --no-dev --no-interaction --no-scripts --optimize-autoloader
-else
-    composer install --no-interaction --no-scripts
-fi
+composer install --no-dev --no-interaction --no-scripts --optimize-autoloader
 
 echo "Generating app key..."
 # Generate an application key
-php artisan key:generate --force
+php artisan key:generate
 echo "App key generated."
 
 # Clear the optimization cache
@@ -52,12 +23,12 @@ rm -rf public/storage
 # Build up a new storage link
 php artisan storage:link
 
-# Check if running in production or development mode
-if [ "$PRODUCTION" = "1" ]; then
-    echo "Running in production mode"
-else
-    echo "Running in development mode"
-fi
+# Cache the configuration, routes, and views
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Running in production mode"
 
 # Start supervisord
 exec /usr/bin/supervisord -c /etc/supervisord.conf
